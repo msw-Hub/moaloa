253d0c777c5105913dbcc6ec255b6c094c68e9b4
sdc9787 <sfc9787@gmail.com>
2024-12-14 14:02:48 +0900

    api주소 변경

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -13,14 +13,4 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
   },
-  server: {
-    proxy: {
-      "/api": {
-        // 예시로 '/api' 경로가 포함된 요청을 프록시
-        target: "http://localhost:8080", // Spring 서버 주소
-        changeOrigin: true,
-        secure: false, // https 사용 시 false 설정
-      },
-    },
-  },
 });
