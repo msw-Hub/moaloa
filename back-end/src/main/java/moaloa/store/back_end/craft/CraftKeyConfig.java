4dbb90521380110d4ecf549a234c8f83aaf60929
msw-Hub <swmoon7@naver.com>
2024-12-15 06:27:53 +0900

    키 오류 수정중

--- back-end/src/main/java/moaloa/store/back_end/craft/CraftKeyConfig.java
+++ back-end/src/main/java/moaloa/store/back_end/craft/CraftKeyConfig.java
@@ -11,8 +11,9 @@ public class CraftKeyConfig {
     public String[] craftApi(
             @Value("${loa.craft.key0}") String craftKey0,
             @Value("${loa.craft.key6}") String craftKey6,
-            @Value("${loa.craft.key7}") String craftKey7
+            @Value("${loa.craft.key7}") String craftKey7,
+            @Value("${loa.craft.key8}") String craftKey8
     ) {
-        return new String[]{craftKey0+craftKey6, craftKey0+craftKey7};
+        return new String[]{craftKey0+craftKey6, craftKey0+craftKey7, craftKey0+craftKey8};
     }
 }
